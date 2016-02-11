adb06380b282fd57f5a8583949d57f9ee6ffa7fd
Jota Perez <yojota@gmail.com>
2016-02-11 18:21:04 -0300

    Siguiendo el tutorial

---
+++ js/script.js
@@ -0,0 +1,37 @@
+var app=angular.module("app",[]);
+  
+app.controller("SeguroController",['$scope','$log','$http',function($scope,$log,$http) {
+  $scope.seguro={
+    nif:"",
+    nombre:"",
+    ape1:"",
+    edad:undefined,
+    sexo:"",
+    casado:false,
+    numHijos:undefined,
+    embarazada:false,
+    coberturas: {
+      oftalmologia:false,
+      dental:false,
+      fecundacionInVitro:false
+    },
+    enfermedades:{
+      corazon:false,
+      estomacal:false,
+      rinyones:false,
+      alergia:false,
+      nombreAlergia:""
+    },
+    fechaCreacion:new Date()
+  }
+  $log.debug("Acabamos de crear el $scope");
+
+  $http({
+    method: 'GET', 
+    url: 'datos.json'
+  }).success(function(data, status, headers, config) {
+      $scope.seguro=data;
+  }).error(function(data, status, headers, config) {
+      alert("Ha fallado la petición. Estado HTTP:"+status);
+  });
+}]);
